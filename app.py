5178ddd0882e0c8d22eb9251f96769e9daed22fe
khushwant singh <khushwantsingh1916@gmail.com>
2025-03-08 22:20:14 +0530

    finally

--- app.py
+++ app.py
@@ -17,11 +17,6 @@ google = oauth.remote_app(
     request_token_params={
         'scope': 'email',
     },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
 )
 
 @app.route('/')
